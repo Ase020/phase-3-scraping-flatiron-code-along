a15622611bbd1d3d4b9c0000ea4b5ed1f5c4c186
felixNyalenda <felixasejunior@gmail.com>
2023-05-31 17:31:22 +0300

    completed assignment

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,24 @@
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
+  def initialize
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 
+  def self.reset_all
+    @@all.clear
+  end
+end
 
+c1 = Course.new
+c1.title = "Course1"
+c1.description = 'Desc1'
+
+c2 = Course.new
+c2.title = "Course2"
+c2.description = 'Desc2'
+
+puts Course.all
